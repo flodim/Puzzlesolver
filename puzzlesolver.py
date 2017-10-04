fd828f4f6a29a1c63cf6d044800bdfb945e76131
Dimitri Lizzi <araxor@gmail.com>
2017-10-04 18:58:13 +0200

    Cleanup and type annotations

--- puzzlesolver.py
+++ puzzlesolver.py
@@ -1,11 +1,8 @@
 # /usr/bin/env python3
 from collections import deque
-from queue import Queue, Empty
-from random import shuffle
-from typing import List, Optional, IO
-from io import StringIO
-
 from copy import deepcopy
+from random import shuffle
+from typing import List, Optional, Iterator, Tuple, Iterable
 
 UP = (0, -1)
 DOWN = (0, 1)
@@ -13,7 +10,7 @@ LEFT = (-1, 0)
 RIGHT = (1, 0)
 
 
-class Puzzle:
+class PuzzleState:
     def __init__(
             self, size: int,
             matrix: List[List[int]],
@@ -31,7 +28,7 @@ class Puzzle:
         self.last_state = last_state
 
     @classmethod
-    def from_list(cls, size, values: List[int]):
+    def from_iterable(cls, size: int, values: Iterable[int]) -> 'PuzzleState':
         empty_line = None
         empty_column = None
         matrix = [[-1] * size for _ in range(size)]
@@ -44,37 +41,63 @@ class Puzzle:
         return cls(size, matrix, empty_line, empty_column)
 
     @classmethod
-    def from_random(cls, size):
+    def from_random(cls, size: int) -> 'PuzzleState':
         values = list(range(size * size))
         shuffle(values)
-        return cls.from_list(size, values)
+        return cls.from_iterable(size, values)
 
     @classmethod
-    def from_keyboard(cls):
+    def from_keyboard(cls) -> 'PuzzleState':
         size = int(input('enter matrix size: '))
         values = list(range(size))
         for i in range(0, size * size):
             values[i] = int(input("enter value " + str(i) + ": "))
-        return cls.from_list(size, values)
-
-    def move(self, direction):
-        dx, dy = direction
-        target_line = self.empty_line + dy
-        target_column = self.empty_column + dx
-        target_value = self.matrix[target_line][target_column]
-        new_matrix = deepcopy(self.matrix)
-        new_matrix[target_line][target_column] = 0
-        new_matrix[self.empty_line][self.empty_column] = target_value
-        new_empty_line = target_line
-        new_empty_column = target_column
-        return Puzzle(self.size, new_matrix, new_empty_line, new_empty_column, direction, self)
+        return cls.from_iterable(size, values)
 
     @property
-    def values(self):
+    def values(self) -> Tuple:
         values = tuple([self.matrix[l][c] for l, c in self.matrix_indices])
         return tuple(values)
 
-    def __str__(self):
+    @property
+    def successors(self) -> List['PuzzleState']:
+        return [self.move(direction)
+                for direction in [UP, DOWN, LEFT, RIGHT]
+                if self.can_move(direction)]
+
+    @property
+    def matrix_indices(self) -> Iterator[Tuple[int, int]]:
+        return ((line, column)
+                for line in range(self.size)
+                for column in range(self.size))
+
+    @property
+    def nb_correct(self) -> int:
+        correct_cells = sum(1 for l, c in self.matrix_indices
+                            if self.cell_is_correct(l, c))
+        return correct_cells
+
+    @property
+    def is_solved(self) -> bool:
+        return self.nb_correct == self.nb_values
+
+    @property
+    def path(self) -> List['PuzzleState']:
+        inverted_path = [self]
+        current = self
+        while current.last_state is not None:
+            inverted_path.append(current.last_state)
+            current = current.last_state
+        return list(reversed(inverted_path))
+
+    @property
+    def total_manhattan_distance(self) -> int:
+        distances = (self.manhattan_distance(l, c)
+                     for l, c in self.matrix_indices)
+        total = sum(distances)
+        return total
+
+    def __str__(self) -> str:
         string = "----------\n"
         for line in self.matrix:
             for value in line:
@@ -83,17 +106,17 @@ class Puzzle:
         string += "----------"
         return string
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'PuzzleState') -> bool:
         return self.size == other.size and self.__hash__() == other.__hash__()
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.values)
 
-    def cell_is_correct(self, line, column):
+    def cell_is_correct(self, line: int, column: int) -> bool:
         expected = line * self.size + column + 1
         return self.matrix[line][column] == expected
 
-    def can_move(self, direction):
+    def can_move(self, direction: Tuple[int, int]) -> bool:
         if direction == self.last_move:
             return False
         if direction == UP:
@@ -105,38 +128,7 @@ class Puzzle:
         if direction == RIGHT:
             return self.empty_column < self.size - 1
 
-    @property
-    def successors(self):
-        return [self.move(direction)
-                for direction in [UP, DOWN, LEFT, RIGHT]
-                if self.can_move(direction)]
-
-    @property
-    def matrix_indices(self):
-        return ((line, column)
-                for line in range(self.size)
-                for column in range(self.size))
-
-    @property
-    def nb_correct(self):
-        correct_cells = sum(1 for l, c in self.matrix_indices
-                            if self.cell_is_correct(l, c))
-        return correct_cells
-
-    @property
-    def is_solved(self):
-        return self.nb_correct == self.nb_values
-
-    @property
-    def path(self):
-        inverted_path = [self]
-        current = self
-        while current.last_state is not None:
-            inverted_path.append(current.last_state)
-            current = current.last_state
-        return list(reversed(inverted_path))
-
-    def manhattan_distance(self, line: int, column: int):
+    def manhattan_distance(self, line: int, column: int) -> int:
         value = self.matrix[line][column]
         expected_line, expected_column = divmod(value - 1, self.size)
         v_distance = abs(line - expected_line)
@@ -144,15 +136,20 @@ class Puzzle:
         distance = v_distance + h_distance
         return distance
 
-    @property
-    def total_manhattan_distance(self):
-        distances = (self.manhattan_distance(l, c)
-                     for l, c in self.matrix_indices)
-        total = sum(distances)
-        return total
+    def move(self, direction: Tuple(int, int)) -> 'PuzzleState':
+        dx, dy = direction
+        target_line = self.empty_line + dy
+        target_column = self.empty_column + dx
+        target_value = self.matrix[target_line][target_column]
+        new_matrix = deepcopy(self.matrix)
+        new_matrix[target_line][target_column] = 0
+        new_matrix[self.empty_line][self.empty_column] = target_value
+        new_empty_line = target_line
+        new_empty_column = target_column
+        return PuzzleState(self.size, new_matrix, new_empty_line, new_empty_column, direction, self)
 
 
-def solve(initial_puzzle: Puzzle):
+def solve(initial_puzzle: PuzzleState) -> Optional[List[PuzzleState]]:
     if initial_puzzle.is_solved:
         return [initial_puzzle]
 
@@ -174,7 +171,7 @@ def solve(initial_puzzle: Puzzle):
 
 
 if __name__ == '__main__':
-    puzzle = Puzzle.from_list(3, [5, 4, 0, 6, 1, 8, 7, 3, 2])
+    puzzle = PuzzleState.from_iterable(3, [5, 4, 0, 6, 1, 8, 7, 3, 2])
     solution = solve(puzzle)
 
     if solution:
