1400dda4136f0c8250dd107401be0e2c38b4e2ec
floxblah <florian.hassler1989@gmail.com>
2017-10-27 14:30:22 +0200

    implement solve_backtracking

--- sudokusolver.py
+++ sudokusolver.py
@@ -4,6 +4,12 @@ from typing import List, Optional, Iterator, Tuple, Iterable
 
 import math
 
+EMPTY = -1
+
+
+def check_row():
+    return 0
+
 
 class Sudoku:
     def __init__(self, size: int,
@@ -11,11 +17,12 @@ class Sudoku:
 
         self.size = size
         self.matrix = matrix
+        self.block_size = int(math.sqrt(size))
 
     @classmethod
     def from_file(cls, file_path: str, size: int):
         file = open(file_path, "r")
-        matrix = [[-1] * (size) for _ in range(size)]
+        matrix = [[-1] * size for _ in range(size)]
         l = 0
         for line in file:
             c = 0
@@ -29,20 +36,77 @@ class Sudoku:
             l += 1
         return cls(size, matrix)
 
+    @property
+    def empty_squares(self) -> List[Tuple[int, int]]:
+        empty_squares = []
+        for row in range(self.size):
+            for col in range(self.size):
+                if self.matrix[row][col] == EMPTY:
+                    empty_squares.append([row, col])
+        return empty_squares
+
+    def solve_heuristic(self) -> bool:
+        # TODO: implement this
+        return False
+
+    def solve_forward_checking(self) -> bool:
+        return False;
+
+    def solve_backtracking(self) -> bool:
+        if len(self.empty_squares) == 0:
+            return True
+        possible_values = list(range(1, 10))
+        square=self.empty_squares[0]
+        row = square[0]
+        col = square[1]
+        while len(possible_values) != 0:
+            value = possible_values[0]
+            possible_values.remove(value)
+            if self.check_column(value, row) and \
+                    self.check_row(value, col) and\
+                    self.check_block(value,square):
+                self.matrix[row][col] = value
+                if self.solve_backtracking():
+                    return True
+                else:
+                    self.matrix[row][col] = EMPTY
+        return False
+
+    def check_column(self, value: int, row: int) -> bool:
+        for col in range(self.size):
+            if self.matrix[row][col] == value:
+                return False
+        return True
+
+    def check_row(self, value: int, col: int) -> bool:
+        for row in range(self.size):
+            if self.matrix[row][col] == value:
+                return False
+        return True
+
+    def check_block(self, value, square: Tuple[int, int]) -> bool:
+        row= square[0]-(square[0]%self.block_size);
+        col = square[1] - (square[1] % self.block_size);
+
+        for i in range(row,row+(self.block_size)):
+            for j in range(col,col+(self.block_size)):
+                if self.matrix[i][j] == value:
+                    return False
+        return True
+
     def __str__(self) -> str:
         string = ""
         space = "  "
-        long_line = " ___________________________________\n"
-        sub_array_size = math.sqrt(self.size)
+        long_line = "-------------------------------------\n"
         l = 0
         for line in self.matrix:
             c = 0
-            if l % sub_array_size == 0:
+            if l % self.block_size == 0:
                 string += long_line
             for value in line:
-                if c % sub_array_size == 0:
+                if c % self.block_size == 0:
                     string += "|" + space
-                string += str(value) + space if value != -1 else '-' + space
+                string += str(value) + space if value != EMPTY else '-' + space
                 c += 1
             string += '|\n'
             l += 1
@@ -50,16 +114,9 @@ class Sudoku:
         return string
 
 
-def solve_forward_checking():
-    # TODO: implement this
-    return -1
-
-
-def solve_heuristic():
-    # TODO: implement this
-    return -1
-
-
 if __name__ == '__main__':
     s = Sudoku.from_file('./sudoku.txt', 9)
+    print(s)
+    s.solve_backtracking()
+    print("solve backtracking:\n")
     print(s)
